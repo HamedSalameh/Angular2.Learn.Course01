5d28adaa7a3d76757eb86df358ab76f4f1f71ff3
hamedsalameh <hamedsalami@gmail.com>
2017-04-23 05:16:06 +0300

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
+
     selector: 'pm-app',
-    template:`
-    <div>
-        <h1>
-            {{pageTitle}}
-        </h1>
-        <div>Let's Rock NG2 !</div>
-    </div>
+    template:
     `
     
+    <div>
+        <h1>angular 2</h1>
+        <div>
+            Let's Rock NG2 - NG4
+        </div>
+    </div>
+
+    `
+
 })
 
-export class AppComponent {
-    pageTitle: string = "My Angular2 App"
-}
+export class AppComponent {}
